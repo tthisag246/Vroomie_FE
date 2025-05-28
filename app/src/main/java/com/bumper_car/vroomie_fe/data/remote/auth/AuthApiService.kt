e9050c7bfa8b766ec3a96fb6536d7b706c44db02
Seokwoo <seokwoo1999@naver.com>
2025-05-28 17:09:42 +0900

    chore: 클래스명 및 변수명 변경

--- app/src/main/java/com/bumper_car/vroomie_fe/data/remote/auth/AuthApiService.kt
+++ app/src/main/java/com/bumper_car/vroomie_fe/data/remote/auth/AuthApiService.kt
@@ -3,14 +3,14 @@ package com.bumper_car.vroomie_fe.data.remote.auth
 import retrofit2.http.GET
 import retrofit2.http.Header
 
-interface MeApiService {
+interface AuthApiService {
     @GET("/me")
-    suspend fun getMe(
+    suspend fun getMyInfo(
         @Header("Authorization") token: String
-    ): MeResponse
+    ): GetMyInfoResponse
 }
 
-data class MeResponse(
+data class GetMyInfoResponse(
     val kakao_id: String,
-    val nickname: String
+    val user_name: String
 )
